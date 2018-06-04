84328423aa1d0c9d19f1401adb0cb8990e98a370
Nguyen Manh Hung <thjngking846@gmail.com>
2018-06-04 20:41:22 +0700

    ADD NEW NOTE

--- MyNote/NoteManegement/FormAdd.cs
+++ MyNote/NoteManegement/FormAdd.cs
@@ -17,6 +17,50 @@ namespace MyNote.NoteManegement
             InitializeComponent();
         }
 
-        
+        private void FormAdd_Load(object sender, EventArgs e)
+        {
+            var db = new MyNoteEntities7();
+            radioButton1.Checked = true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            String sStatus = "";
+            if (radioButton1.Checked)
+            {
+                sStatus = radioButton1.Text;
+            }
+            if (radioButton2.Checked)
+            {
+                sStatus = radioButton2.Text;
+            }
+            if (radioButton3.Checked)
+            {
+                sStatus = radioButton3.Text;
+            }
+
+            Note @note = new Note();
+            @note.title = this.txTitle.Text;
+            @note.notecontent = this.txContent.Text;
+            @note.Datetodo = this.dtpDate.Value;
+            @note.status = sStatus;
+            try
+            {
+                var db = new MyNoteEntities7();
+                db.Notes.Add(@note);
+                db.SaveChanges();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
     }
 }
